5b13a785eabec942175831f5a4e47ab71ff64953
leumu <samuel.chua1@yahoo.com>
2025-10-21 16:27:56 +0800

    Add Attend parser logic

---
+++ src/main/java/seedu/address/logic/commands/AttendCommand.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Marks attendance for an existing person in the address book.
+ */
+public class AttendCommand extends Command {
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+}
