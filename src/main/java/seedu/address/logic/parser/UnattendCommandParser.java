a43f69db74f648329f0b2b3e32c05db8266c800e
leumu <samuel.chua1@yahoo.com>
2025-10-22 03:29:18 +0800

    Add Unattend command and parser logic

--- src/main/java/seedu/address/logic/parser/UnattendCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnattendCommandParser.java
@@ -6,22 +6,22 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIALCLASS;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.AttendCommand;
+import seedu.address.logic.commands.UnattendCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.TutorialClass;
 
 /**
- * Parses input arguments and creates a new AttendCommand object
+ * Parses input arguments and creates a new UnattendCommand object
  */
-public class AttendCommandParser implements Parser<AttendCommand> {
+public class UnattendCommandParser implements Parser<UnattendCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AttendCommand
-     * and returns an AttendCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the UnattendCommand
+     * and returns an UnattendCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
-    public AttendCommand parse(String args) throws ParseException {
+    public UnattendCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
                 PREFIX_TUTORIALCLASS);
@@ -36,12 +36,12 @@ public class AttendCommandParser implements Parser<AttendCommand> {
             tutClass = ParserUtil.parseTutorialClass(tutorialClassName);
         } catch (IllegalValueException | NumberFormatException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AttendCommand.MESSAGE_USAGE), ive);
+                    UnattendCommand.MESSAGE_USAGE), ive);
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TUTORIALCLASS);
 
-        return new AttendCommand(index, tutClass);
+        return new UnattendCommand(index, tutClass);
     }
 
 }
