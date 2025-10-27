07fa4bb68c727e40fb271d04e9a701953374c131
leumu <samuel.chua1@yahoo.com>
2025-10-28 00:42:00 +0800

    Update user-input for attend and unattend

--- src/main/java/seedu/address/logic/parser/AttendCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AttendCommandParser.java
@@ -34,8 +34,9 @@ public class AttendCommandParser implements Parser<AttendCommand> {
 
             assert index.getOneBased() > 0;
 
-            String tutorialClassName = argMultimap.getValue(PREFIX_TUTORIALCLASS).orElse("");
-            tutClass = ParserUtil.parseTutorialClass(tutorialClassName);
+            String tutClassName = argMultimap.getValue(PREFIX_TUTORIALCLASS).orElse("");
+            String tutClassNameLower = tutClassName.toLowerCase();
+            tutClass = ParserUtil.parseTutorialClass(tutClassNameLower);
         } catch (IllegalValueException | NumberFormatException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     AttendCommand.MESSAGE_USAGE), ive);
