353e021e2c294d9f221abd06b689ecccba1eefc2
salmonkarp <garyaxelmuliyono@gmail.com>
2025-10-27 19:48:52 +0800

    Replace assert statements

--- src/test/java/seedu/address/model/person/AssignmentsTest.java
+++ src/test/java/seedu/address/model/person/AssignmentsTest.java
@@ -1,6 +1,9 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,14 +16,14 @@ public class AssignmentsTest {
     @Test
     public void fromString_validInput_success() {
         for (Assignments gradeType : Assignments.values()) {
-            assert(Assignments.fromString(gradeType.name()) == gradeType);
+            assertEquals(Assignments.fromString(gradeType.name()), gradeType);
         }
     }
 
     @Test
     public void fromString_caseInsensitiveInput_success() {
-        assert(Assignments.fromString(VALID_ASSIGNMENT_Q1_CASE_INSENSITIVE) == Assignments.Q1);
-        assert(Assignments.fromString(VALID_ASSIGNMENT_FINALS_CASE_INSENSITIVE) == Assignments.Finals);
+        assertEquals(Assignments.Q1, Assignments.fromString(VALID_ASSIGNMENT_Q1_CASE_INSENSITIVE));
+        assertEquals(Assignments.Finals, Assignments.fromString(VALID_ASSIGNMENT_FINALS_CASE_INSENSITIVE));
     }
 
     @Test
@@ -32,22 +35,22 @@ public class AssignmentsTest {
     public void isValidAssignment() {
         // valid assignments
         for (Assignments assignment : Assignments.values()) {
-            assert(Assignments.isValidAssignment(assignment.name()));
-            assert(Assignments.isValidAssignment(assignment.name().toLowerCase()));
-            assert(Assignments.isValidAssignment(assignment.name().toUpperCase()));
+            assertTrue(Assignments.isValidAssignment(assignment.name()));
+            assertTrue(Assignments.isValidAssignment(assignment.name().toLowerCase()));
+            assertTrue(Assignments.isValidAssignment(assignment.name().toUpperCase()));
         }
 
         // invalid assignments
-        assert(!Assignments.isValidAssignment(INVALID_ASSIGNMENT));
-        assert(!Assignments.isValidAssignment(""));
+        assertFalse(Assignments.isValidAssignment(INVALID_ASSIGNMENT));
+        assertFalse(Assignments.isValidAssignment(""));
     }
 
     @Test
     public void getAllAssignments_returnsAllAssignments() {
         Assignments[] allAssignments = Assignments.getAllAssignments();
-        assert(allAssignments.length == Assignments.values().length);
+        assertEquals(allAssignments.length, Assignments.values().length);
         for (int i = 0; i < allAssignments.length; i++) {
-            assert(allAssignments[i] == Assignments.values()[i]);
+            assertEquals(allAssignments[i], Assignments.values()[i]);
         }
     }
 }
