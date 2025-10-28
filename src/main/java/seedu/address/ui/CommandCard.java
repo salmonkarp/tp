1638dbbef02b9fdb96f9b6695fe5a8a67c3444cc
Leifsen <leifsenlaw993@gmail.com>
2025-10-28 12:08:58 +0800

    Update CommandCard to utilize text instead of label

--- src/main/java/seedu/address/ui/CommandCard.java
+++ src/main/java/seedu/address/ui/CommandCard.java
@@ -1,17 +1,23 @@
 package seedu.address.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
+/**
+ * A UI component that displays information of a single command.
+ */
 public class CommandCard extends UiPart<Region> {
     public static final String FXML = "CommandCard.fxml";
 
     @FXML
-    private Label commandLabel;
+    private TextField commandField;
 
+    /**
+     * Creates a {@code CommandCard} with the given {@code commandText}.
+     */
     public CommandCard(String commandText) {
         super(FXML);
-        commandLabel.setText(commandText);
+        commandField.setText(commandText);
     }
 }
