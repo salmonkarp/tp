d4dabdb50c24f1ff7882895c4e81d3bdbaed9eb6
Leifsen <leifsenlaw993@gmail.com>
2025-10-29 21:30:41 +0800

    Delete unused imports

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -9,8 +9,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.ui.OpenAlert;
-import javafx.scene.control.Label;
 
 public class ClearCommandTest {
 
