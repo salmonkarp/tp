e60a9fc97345ab3e9c8b09d01c0c7b539e22e1cf
leumu <samuel.chua1@yahoo.com>
2025-10-29 19:02:15 +0800

    Update Delete error message

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -18,13 +18,10 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
-        }
+        // Removed try-catch block here as by right, all the parsing errors will be handled in the respective.
+        // ParserUtil methods called below and throw ParseException with relevant messages.
+        Index index = ParserUtil.parseIndex(args);
+        return new DeleteCommand(index);
     }
 
 }
