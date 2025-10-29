9d220cc636dabb1840de1c7f173c60843020edb6
Leifsen <leifsenlaw993@gmail.com>
2025-10-29 19:32:12 +0800

    Modify ClearCommand to open an Alert to double confirm

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.ui.OpenAlert;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
@@ -15,10 +16,34 @@ public class ClearCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
 
+    /**
+     * Functional interface for confirmation dialog. Tests can implement this to simulate user confirmation.
+     */
+    public interface ConfirmationProvider {
+        boolean confirm(String title, String content);
+    }
+
+    private final ConfirmationProvider confirmationProvider;
+
+    public ClearCommand() {
+        this(OpenAlert::showAlert);
+    }
+
+    public ClearCommand(ConfirmationProvider confirmationProvider) {
+        this.confirmationProvider = confirmationProvider;
+    }
+
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        boolean isConfirmed = confirmationProvider.confirm(
+                "Confirm Clear",
+                "All data will be lost. Are you sure you want to proceed?"
+        );
+        if (isConfirmed) {
+            model.setAddressBook(new AddressBook());
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
+        return new CommandResult("Clear command cancelled.");
     }
 }
